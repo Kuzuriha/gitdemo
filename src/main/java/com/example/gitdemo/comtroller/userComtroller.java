3962d5965f644ae0f53251fc45cd4d5e6ba87bf6
Kuzuriha <a965309722@gmail.com>
2026-03-23 14:48:08 +0800

    提交

--- src/main/java/com/example/gitdemo/comtroller/userComtroller.java
+++ src/main/java/com/example/gitdemo/comtroller/userComtroller.java
@@ -4,4 +4,16 @@ public class userComtroller {
     public String index(){
         return "index";
     }
+    public String login(){
+        return "login";
+    }
+    public String register(){
+        return "register";
+    }
+    public String logout(){
+        return "logout";
+    }
+    public String user(){
+        return "user";
+    }
 }
---
+++ src/main/java/com/example/gitdemo/serverce/userServerce.java
@@ -0,0 +1,16 @@
+package com.example.gitdemo.serverce;
+
+public class userServerce {
+    public String index(){
+        return "index";
+    }
+    public String index2(){
+        return "index2";
+    }
+    public String index3(){
+        return "index3";
+    }
+    public String index4(){
+        return "index4";
+    }
+}
